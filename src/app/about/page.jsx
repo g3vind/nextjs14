9477305b32970ce74577462af8739742ef7d4c61
g3vind <g3vind@gmail.com>
2024-01-18 00:19:15 +0530

    components setup

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
+const AboutPage = () => {
   return <div>AboutPage</div>;
 };
 
-export default page;
+export default AboutPage;
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+
+const Links = () => {
+  const links = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blogs",
+      path: "/blog",
+    },
+  ];
+  return (
+    <div>
+      {links.map((link) => (
+        <Link href={link.path} key={link.title}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
